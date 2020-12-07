ddf59229d1d92d9e675d0a0b5328bb52911fc279
Daniel Kao <daniel.kao@gmail.com>
2020-12-08 01:38:55 +0800

    add html tags so that it can be converted to epub more easily.

--- convert.py
+++ convert.py
@@ -21,33 +21,36 @@ def convert_file(file_name, main_lang, sub_lang):
   index_sub = 0
   while index_main < len(vtt_main):
     while index_sub < len(vtt_sub):
-      caption_k = vtt_main[index_main]
-      caption_z = vtt_sub[index_sub]
+      caption_main = vtt_main[index_main]
+      caption_sub = vtt_sub[index_sub]
 
-      if (caption_k.start < caption_z.start):
-        print(caption_k.text)
+      if (caption_main.start < caption_sub.start):
+        #print("##### " + caption_main.text.replace("&lrm;","").replace("\n","\n##### "))
+        print("<h3>" + caption_main.text.replace("&lrm;","") + "</h3>")
         break
       else:
-        print(caption_z.text)
-        print("")
+        print("<p>" + caption_sub.text.replace("&lrm;", ""))
+        print("<p>")
         index_sub += 1
     index_main += 1
 
   # finish final z index
   while index_sub < len(vtt_sub):
-    print(caption_z.text)
+    print(caption_sub.text)
     index_sub += 1
 
-
-  print('end of ' + file_name)
+  #print('end of ' + file_name)
 
 # main
+print("<html>")
 lang_main = sys.argv[1]
 lang_sub = sys.argv[2]
 for i in range(3, len(sys.argv)):
   print('<title>Episode' + str(i - 2) + '</title>')
-  print('======================')
-  print('Episode ' + str(i - 2))
-  print('======================')
+  print('<p>======================')
+  print('<p>Episode ' + str(i - 2))
+  print('<p>======================')
   print('')
   convert_file(sys.argv[i], lang_main, lang_sub)
+
+print("</html>")
