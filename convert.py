ed6abf3d231d20110de29f412487bb26b3962ea9
Daniel Kao <daniel.kao@gmail.com>
2020-12-07 22:06:07 +0800

    support setting language main and sub and original filenames

--- convert.py
+++ convert.py
@@ -1,21 +1,28 @@
 import webvtt
 import sys
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 4:
   print("need file names")
   sys.exit(1)
 
-def convert_file(file_name):
-  vtt_k = webvtt.read(file_name[1:])
-  vtt_z = webvtt.read(file_name)
+def rreplace(s, old, new):
+  li = s.rsplit(old, 1)
+  return new.join(li)
+
+def convert_file(file_name, main_lang, sub_lang):
+  file_name_sub = rreplace(file_name, main_lang, sub_lang)
+  #print(file_name + '\n' + file_name_sub)
+  #return
+  vtt_main = webvtt.read(file_name)
+  vtt_sub = webvtt.read(file_name_sub)
 
   # while loop all korean time captions
-  index_k = 0
-  index_z = 0
-  while index_k < len(vtt_k):
-    while index_z < len(vtt_z):
-      caption_k = vtt_k[index_k]
-      caption_z = vtt_z[index_z]
+  index_main = 0
+  index_sub = 0
+  while index_main < len(vtt_main):
+    while index_sub < len(vtt_sub):
+      caption_k = vtt_main[index_main]
+      caption_z = vtt_sub[index_sub]
 
       if (caption_k.start < caption_z.start):
         print(caption_k.text)
@@ -23,21 +30,24 @@ def convert_file(file_name):
       else:
         print(caption_z.text)
         print("")
-        index_z += 1
-    index_k += 1
+        index_sub += 1
+    index_main += 1
 
   # finish final z index
-  while index_z < len(vtt_z):
+  while index_sub < len(vtt_sub):
     print(caption_z.text)
-    index_z += 1
+    index_sub += 1
 
 
   print('end of ' + file_name)
 
-for i in range(1, len(sys.argv)):
-  print('<title>第' + str(i) + '集</title>')
+# main
+lang_main = sys.argv[1]
+lang_sub = sys.argv[2]
+for i in range(3, len(sys.argv)):
+  print('<title>Episode' + str(i - 2) + '</title>')
   print('======================')
-  print('第' + str(i) + '集')
+  print('Episode ' + str(i - 2))
   print('======================')
   print('')
-  convert_file(sys.argv[i])
+  convert_file(sys.argv[i], lang_main, lang_sub)
