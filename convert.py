3fff75633ec63fd5fa0ef1ffe77a18577ce03219
Julian_Chu <walkingice0204@gmail.com>
2020-12-12 01:05:59 +0800

    convert: apply argparse to parse arguments
    $ python convert.py lang_main lang_sub path_to_subtitles/*\[cc\].vtt > output.html
    
    or
    
    $ python convert.py lang_main lang_sub path_to_subtitles/*\[cc\].vtt -o output.html

--- convert.py
+++ convert.py
@@ -1,9 +1,18 @@
 import webvtt
 import sys
+import argparse
 
-if len(sys.argv) < 4:
-  print("need file names")
-  sys.exit(1)
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', dest="output", metavar="OUTPUT.html", type=argparse.FileType('w', encoding='UTF-8'), default=sys.stdout, help="write to output file")
+parser.add_argument("main_lang", type=str, help="main language")
+parser.add_argument("sub_lang", type=str, help="sub language")
+parser.add_argument('input_files', nargs='*', type=str, help="/path/to/*\[cc\].vtt")
+
+args = parser.parse_args()
+
+def write(str):
+    args.output.write(str)
+    args.output.write("\n")
 
 def rreplace(s, old, new):
   li = s.rsplit(old, 1)
@@ -11,7 +20,7 @@ def rreplace(s, old, new):
 
 def convert_file(file_name, main_lang, sub_lang):
   file_name_sub = rreplace(file_name, main_lang, sub_lang)
-  #print(file_name + '\n' + file_name_sub)
+  #write(file_name + '\n' + file_name_sub)
   #return
   vtt_main = webvtt.read(file_name)
   vtt_sub = webvtt.read(file_name_sub)
@@ -24,33 +33,31 @@ def convert_file(file_name, main_lang, sub_lang):
       caption_main = vtt_main[index_main]
       caption_sub = vtt_sub[index_sub]
 
-      if (caption_main.start < caption_sub.start):
-        #print("##### " + caption_main.text.replace("&lrm;","").replace("\n","\n##### "))
-        print("<h3>" + caption_main.text.replace("&lrm;","") + "</h3>")
+      if (caption_main.start <= caption_sub.start):
+        #write("##### " + caption_main.text.replace("&lrm;","").replace("\n","\n##### "))
+        write("<h3>" + caption_main.text.replace("&lrm;","") + "</h3>")
         break
       else:
-        print("<p>" + caption_sub.text.replace("&lrm;", ""))
-        print("<p>")
+        write("<p>" + caption_sub.text.replace("&lrm;", ""))
+        write("<p>")
         index_sub += 1
     index_main += 1
 
   # finish final z index
   while index_sub < len(vtt_sub):
-    print(caption_sub.text)
+    write(caption_sub.text)
     index_sub += 1
 
-  #print('end of ' + file_name)
+  #write('end of ' + file_name)
 
 # main
-print("<html>")
-lang_main = sys.argv[1]
-lang_sub = sys.argv[2]
-for i in range(3, len(sys.argv)):
-  print('<title>Episode' + str(i - 2) + '</title>')
-  print('<p>======================')
-  print('<p>Episode ' + str(i - 2))
-  print('<p>======================')
-  print('')
-  convert_file(sys.argv[i], lang_main, lang_sub)
+write("<html>")
+for idx, vtt_file in enumerate(args.input_files):
+  write('<title>Episode' + str(idx + 1) + '</title>')
+  write('<p>======================')
+  write('<p>Episode ' + str(idx + 1))
+  write('<p>======================')
+  write('')
+  convert_file(vtt_file, args.main_lang, args.sub_lang)
 
-print("</html>")
+write("</html>")
